cbb3d039207ab02d5dac16a0c0544779e8af7e0a
Austin Joiner <austin.j.joiner1@gmail.com>
2023-01-25 17:01:00 -0500

    fixed error on first test

--- tests.js
+++ tests.js
@@ -15,7 +15,7 @@ describe('helloWorld', function() {
 });
 
 describe('sayHello', function() {
-    it("should be a defined function", function() {
+    it("should be a defined function", function () {
         expect(typeof sayHello).toBe("function");
     });
-
+});
